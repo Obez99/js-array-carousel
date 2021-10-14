83039d397d37c365941e90fa224ecfa65a185e97
Obez99 <giuliogx@live.it>
2021-10-14 22:04:56 +0200

    Adds scroll function to carousel

--- js/scripts.js
+++ js/scripts.js
@@ -26,21 +26,59 @@ const text = [
 //************************************************* */
 
 const carouselSideElement = document.querySelector(".small_images_container");
-const arrowUpElement = document.querySelector("arrow_up");
-const arrowDownElement = document.querySelector("arrow_down");
-
-
-
 
 for (let i = 0; i < items.length; i++) {
-  let activeStatus = "";
-  if (i === 0) {
-    activeStatus = "active";
-  }
-
-  const smallImage = `<div class="small_image ${activeStatus}"><img src=${items[i]} alt=${title[i]}-img></img></div>`;
+  const smallImage = `<div class="small_image ${i === 0 ? "active" : ""}"><img src=${items[i]} alt=${title[i]}-img></img></div>`;
   carouselSideElement.innerHTML += smallImage;
 }
+
+const arrowUp = `<div class="arrow arrow_up"><i class="fas fa-chevron-up"></i></div>`;
+carouselSideElement.innerHTML += arrowUp;
+const arrowDown = `<div class="arrow arrow_down"><i class="fas fa-chevron-down"></i></div>`;
+carouselSideElement.innerHTML += arrowDown;
+
+const arrowUpElement = document.querySelector(".arrow_up");
+const arrowDownElement = document.querySelector(".arrow_down");
+const smallImageElement = document.querySelectorAll(".small_image");
+const jumboElement = document.getElementById("jumbo_img");
+let jumboTitle = document.querySelector(".image_title");
+let jumboDescription = document.querySelector(".image_description");
+let index = 0;
+
+jumboElement.src = items[index];
+jumboTitle.innerHTML = title[index];
+jumboDescription.innerHTML = text[index];
+
+console.log("smallImageElement", smallImageElement);
+console.log("smallImageElement[0]", smallImageElement[0]);
+
+arrowDownElement.addEventListener("click", function () {
+  console.log(index);
+  smallImageElement[index].classList.remove("active");
+  if (index < 4) index++;
+  else index = 0;
+  smallImageElement[index].classList.add("active");
+
+  jumboElement.src = items[index];
+  jumboTitle.innerHTML = title[index];
+  jumboDescription.innerHTML = text[index];
+
+
+});
+
+arrowUpElement.addEventListener("click", function () {
+  smallImageElement[index].classList.remove("active");
+  if (index > 0) index--;
+  else index = 4;
+  console.log("index", index);
+  smallImageElement[index].classList.add("active");
+
+  jumboElement.src = items[index];
+  jumboTitle.innerHTML = title[index];
+  jumboDescription.innerHTML = text[index];
+
+});
+
 
 
 
